637544958a0dee1b09ee0dcaa92da75892e83ce5
ApoorvWebTech <144144852+ApoorvWebTech@users.noreply.github.com>
2023-12-26 06:37:16 +0000

    update

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,5 @@
-fetch('../footer.html')
+fetch('footer.html')
 .then((response) => response.text())
 .then((data) => {
     document.querySelector("footer").innerHTML = data;
-});
-
-fetch('./header.html')
-.then((response) => response.text())
-.then((data) => {
-    document.querySelector("header").innerHTML = data;
 });
